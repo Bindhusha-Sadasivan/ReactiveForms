812dc38e7be3f9e43b2c97052797de5a3859dbad
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2025-01-25 21:45:29 +0530

    202 & 203 - Reactive setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ReactiveForms';
+  genders:any = ['male','female'];
+  signupForm!:FormGroup;
+
 }
