185ef4aa5bcd9d158d56cdc32745adf067c7f0cb
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2025-01-25 21:54:19 +0530

    204 - Reactive Creating a Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,9 +10,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ReactiveForms';
   genders:any = ['male','female'];
   signupForm!:FormGroup;
 
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.signupForm = new FormGroup({
+      'username' : new FormControl (null),
+      'email' : new FormControl(null),
+      'gender': new FormControl(null)
+    })
+  }
 }
